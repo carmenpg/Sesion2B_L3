ccecf00f6aac5eb79cee6edaa376a6553183aeeb
carmenperezgomez <carmenperezgomez@192.168.1.144>
2024-10-06 16:39:21 +0200

    Empleado editado

--- src/pkg/Empleado.java
+++ src/pkg/Empleado.java
@@ -2,16 +2,16 @@ package pkg;
 
 public class Empleado {
 	
-	public enum TipoEmpleado{Vendedor, Encargado};
+	public enum TipoEmpleado {Vendedor, Encargado};
 	
-	public float calculoNominaBruta(TipoEmpleado tipo, float ventaMes, float horasExtra) {
+	public static float calculoNominaBruta(TipoEmpleado tipo, float ventaMes, float horasExtra) {
 		
 		
 		float salarioBase = 0;
-		float primas;
-		float extras;
+		float primas = 0;
+		float extras = 0;
 		
-		
+		//Determinar el salario base
 		if(tipo ==TipoEmpleado.Encargado) {	
 			
 			salarioBase = 2500;
@@ -38,16 +38,23 @@ public class Empleado {
 		return salarioBase + primas + extras;
 	}
 	
-	public float calculoNominaNeta(float nominaBruta) {
+	public static float calculoNominaNeta(float nominaBruta) {
 		
-		float retencion = 0;
-		if (nominaBruta >2500) {
+		if(nominaBruta <0)
+			nominaBruta = 0;
+		float salarioNeto = 0, retencion = 0;
+	
+		if (nominaBruta >=2500) {
 			retencion = 0.18f;
-		}else if (nominaBruta > 2000) {
+		}else if (nominaBruta > 2100 && nominaBruta < 2500) {
 			retencion = 0.15f;
+		}else if (nominaBruta <= 2100){
+			retencion = 0;
+			
 		}
 		return nominaBruta*(1-retencion);
 	}
 
 }
+
 
--- src/test/EmpleadoTest.java
+++ src/test/EmpleadoTest.java
@@ -7,33 +7,134 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import pkg.Empleado;
+import pkg.Empleado.TipoEmpleado;
 
 class EmpleadoTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
 
 	@Test
 	void testCalculoNominaBruta() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testCalculoNominaNeta() {
-		fail("Not yet implemented");
 	}
+	
+  // Pruebas para el método calculoNominaBruta
 
+		@Test
+		void testCalculoNominaBruta_Vendedor_LimiteInferiorSinPrima(TestInfo info){
+// al ponerlo como parametro junit le proporciona la informacion de la prueba que se esta ejecutando 
+			float expected = 2000;
+			float actual = Empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 999, 0);
+	        assertEquals(expected, actual);
+	    }
+
+	    @Test
+	    void testCalculoNominaBruta_Vendedor_LimiteSuperiorSinPrima(TestInfo  info) {
+	    	float expected = 2100;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1000, 0);
+	        assertEquals(expected, actual);
+	    }
+
+	    @Test
+	    void testCalculoNominaBruta_Vendedor_LimiteInferiorPrima200(TestInfo  info) {
+	    	float expected = 2100;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1499, 0);
+	        assertEquals(expected, actual);
+	    }
+
+	    @Test
+	    void testCalculoNominaBruta_Vendedor_LimiteSuperiorPrima200(TestInfo  info) {
+	    	float expected = 2200;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1500, 0);
+	        assertEquals(expected, actual );
+	    }
+
+	    @Test
+	    void testCalculoNominaBruta_Vendedor_ConHorasExtra(TestInfo  info) {
+	    	float expected = 2230;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1500, 1);
+	        assertEquals(expected, actual);
+	    }
+
+	    // Pruebas para el tipo Encargado
+	    @Test
+	    void testCalculoNominaBruta_Encargado_LimiteInferiorSinPrima(TestInfo  info) {
+	    	float expected = 2100;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Encargado, 999, 0);
+	        assertEquals(expected, actual );
+	    }
+
+	    @Test
+	    void testCalculoNominaBruta_Encargado_LimiteSuperiorSinPrima(TestInfo  info) {
+	    	float expected = 2100;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Encargado, 1000, 0);
+	        assertEquals(expected, actual);
+	    }
+
+	    @Test
+	    void testCalculoNominaBruta_Encargado_LimiteInferiorPrima200(TestInfo  info) {
+	    	float expected = 2600;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Encargado, 1499, 0);
+	        assertEquals(expected, actual );
+	    } 
+
+	    @Test
+	    void testCalculoNominaBruta_Encargado_LimiteSuperiorPrima200(TestInfo  info) {
+	    	float expected = 2700;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Encargado, 1500, 0);
+	        assertEquals(expected, actual );
+	    }
+
+	    @Test
+	    void testCalculoNominaBruta_Encargado_ConHorasExtra(TestInfo  info) {
+	    	float expected = 2730;
+	    	float actual = Empleado.calculoNominaBruta(TipoEmpleado.Encargado, 1500, 1);
+	        assertEquals(expected, actual);
+	    }
+
+	    // Pruebas para el método calculoNominaNeta
+
+	    @Test
+	    void testCalculoNominaNeta_SinRetencion_LimiteInferior(TestInfo  info) {
+	    	float expected = 2099;
+	    	float actual = Empleado.calculoNominaNeta(2099);
+	        assertEquals(expected, actual);
+	    }
+
+	    @Test
+	    void testCalculoNominaNeta_ConRetencion15_LimiteInferior(TestInfo  info) {
+	    	float expected = 1785;
+	    	float actual = Empleado.calculoNominaNeta(1785);
+	        assertEquals(expected, actual);
+	    }
+
+	    @Test
+	    void testCalculoNominaNeta_ConRetencion15_LimiteSuperior(TestInfo  info) {
+	    	float expected = 2499;
+	    	float actual =  Empleado.calculoNominaNeta(2499);
+	        assertEquals(expected, Empleado.calculoNominaNeta(2499), 0.01);
+	    }
+
+	    @Test
+	    void testCalculoNominaNeta_ConRetencion18_Limite(TestInfo  info) {
+	    	float expected = 2050;
+	    	float actual = Empleado.calculoNominaNeta(2050);
+	        assertEquals(expected, actual);
+	    }
+
+	    @Test
+	    void testCalculoNominaNeta_ConRetencion18_JustoPorEncimaDelLimite(TestInfo  info) {
+	    	float expected = 2050;
+	    	float actual = Empleado.calculoNominaNeta(2050);
+	        assertEquals(expected,actual) ;
+	    }
+	
+
+	
 }
